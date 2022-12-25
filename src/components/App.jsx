136bea975361033c81d39feed7ec4502679f443b
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-12-25 22:03:58 +0200

    delete unused vars from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Form } from './Form/Form';
 import { NameList } from './NameList/NameList';
 import { Filter } from './Filter/Filter';
-import { fetchContacts, deleteContact } from 'redux/operations';
+import { fetchContacts } from 'redux/operations';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
+import { useDispatch } from 'react-redux';
+// import axios from 'axios';
 
 // axios.defaults.baseURL = "https://63a427c49704d18da09fd28c.mockapi.io";
 // const deleteTest = axios.get(`/contacts/:9`);
